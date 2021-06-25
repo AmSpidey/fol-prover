6f8ffa38a3688dc918cd956250592bdc87689108
Marta Nowakowska <me.nowakowska@student.uw.edu.pl>
2021-06-25 15:44:31 +0200

    cleanup

--- prover/src/SATSolver.hs
+++ prover/src/SATSolver.hs
@@ -110,15 +110,3 @@ dpSatSolver = solve . ecnf where
               ol = doWhileCan oneLiteral (rt ++ [[]]) rt
               an = doWhileCan affirmNeg (ol ++ [[]]) ol
           in an
-
-sat :: Formula -> Bool
-sat phi = or [ev int phi | int <- functions atoms [True, False]]
-  where atoms = atomicFormulas phi
-        ev :: (Formula -> Bool) -> Formula -> Bool
-        ev int T = True
-        ev int F = False
-        ev int atom@(Rel _ _) = int atom
-        ev int (Not φ) = not (ev int φ)
-        ev int (Or φ ψ) = ev int φ || ev int ψ
-        ev int (And φ ψ) = ev int φ && ev int ψ
-        ev _ φ = error $ "unexpected formula: " ++ show φ
